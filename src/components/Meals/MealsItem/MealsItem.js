7828a6f4a4f8586ad4a8ac494e1a961440d450b0
GustavoGomesDias <treeelevengg@gmail.com>
2021-09-03 04:41:37 -0300

    feat: add input UI components and used in meal item form

--- src/components/Meals/MealsItem/MealsItem.js
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
+import MealsItemForm from './MealsItemForm';
 import classes from './MealsItem.module.css';
 
 const MealsItem = (props) => {
   const price = `$ ${props.price.toFixed(2)}`;
 
   return (
-    <li className={classes.mel}>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.descrption}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealsItemForm id={props.id} />
       </div>
     </li>
   );

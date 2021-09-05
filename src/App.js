8d61638e0e0a9c29b1373d36f8806dca58fe85b9
GustavoGomesDias <treeelevengg@gmail.com>
2021-09-05 01:38:58 -0300

    feat: add show and close cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const  [cartIsShown, setCartIsShwon] = useState(false);
+
+  const showCartHandle = () => {
+    setCartIsShwon(true);
+  };
+
+  const hideCartHandle = () => {
+    setCartIsShwon(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandle} />}
+      <Header onShowCart={showCartHandle} />
       <main>
         <Meals />
       </main>

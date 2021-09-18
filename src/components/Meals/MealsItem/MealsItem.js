62bfe219cdf78e0936bc9391db59f3ff6e8e277e
GustavoGomesDias <treeelevengg@gmail.com>
2021-09-18 04:43:35 -0300

    feat: add count of items in cart

--- src/components/Meals/MealsItem/MealsItem.js
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import CartContext from '../../../store/cart-context';
 import MealsItemForm from './MealsItemForm';
 import classes from './MealsItem.module.css';
 
 const MealsItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$ ${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+  }
 
   return (
     <li className={classes.meal}>
@@ -14,7 +25,7 @@ const MealsItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealsItemForm id={props.id} />
+        <MealsItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/Meals/MealsItem/MealsItemForm.js
+++ src/components/Meals/MealsItem/MealsItemForm.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import Input from '../../UI/Input';
 import classes from './MealsItemForm.module.css';
 
 const MealsItemForm = (props) => {
+	const [amountIsValid, setAmoutIsValid] = useState(true);
+	const amountInputRef = useRef();
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		// Pega o valor passado por ref
+		const enteredAmount = amountInputRef.current.value;
+		// Independendte do tipo do input ser number, ele sempre será string e então é preciso converter
+		const enteredAmountNumber = +enteredAmount;
+
+		if (
+			enteredAmount.trim().length === 0 ||
+			enteredAmountNumber < 1 ||
+			enteredAmountNumber > 5
+		) {
+			setAmoutIsValid(false);
+			return;
+		}
+		props.onAddToCart(enteredAmountNumber);
+	}
+
 	return (
-		<form className={classes.form}>
-			<Input label="Amount" input={{
-				id: 'amount_' + props.id,
-				type: 'number',
-				min: '1',
-				max: '5',
-				step: '1',
-				defaultValue: '1',
+		<form className={classes.form} onSubmit={submitHandler}>
+			<Input
+				ref={amountInputRef}
+				label="Amount"
+				input={{
+					id: 'amount_' + props.id,
+					type: 'number',
+					min: '1',
+					max: '5',
+					step: '1',
+					defaultValue: '1',
 			}} />
 			<button>+ Add</button>
+			{!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
 		</form>
 	);
 }
